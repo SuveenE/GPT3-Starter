9382f84760b604d4f299781d0a976c501f1b2937
SuveenE <suveen.te1@gmail.com>
2022-12-21 14:11:07 +0530

    code

---
+++ pages/api/generate.js
@@ -0,0 +1,47 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// const basePromptPrefix =
+// `
+// Explain CRISPR to a 
+// `
+// ;
+
+// const basePromptSuffix =
+// `
+//   year old who has never heard of it before in 100 words
+// `
+
+// const basePromptEnd =
+// `
+// heard of it before in 100 words
+// `
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  
+  const prompt =
+`
+Explain CRISPR to a ${req.body.userInput} year old who has ${req.body.heardBefore}heard of it before in 100 words. Take something from ${req.body.userField} for an example as it will make the explanation more clear.
+`
+;
+console.log(`API: ${prompt}`)
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${prompt}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,171 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
+import { DateTime } from "luxon";
+import Countdown from 'react-countdown';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [userField, setUserField] = useState('')
+  const [yesClicked, setYesClicked] = useState(false)
+  const [noClicked, setNoClicked] = useState(false)
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  const [heardBefore, setHeardBefore] = useState('never');
+  //console.log(Date.now())
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput, heardBefore, userField }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const onUserChangedField = (event) => {
+    setUserField(event.target.value);
+  };
+
+  const onYesBtnClick = () =>{
+    setHeardBefore('');
+    setYesClicked(true)
+    setNoClicked(false)
+  }
+  
+  const onNoBtnClick = () =>{
+    setHeardBefore('never');
+    setNoClicked(true)
+    setYesClicked(false)
+  }
+  
+  const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+    } else {
+      return <span>{hours}:{minutes}:{seconds}</span>;
+    }
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>WhatNot</title>
       </Head>
-      <div className="container">
-        <div className="header">
+      <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>WhatNot</h1>
           </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+          <div className='icon-row'>
+          <div className='help-icon'>
+            Help
           </div>
+          <div className='settings-icon'>
+            Stats
+          </div>
+          </div>
+          {/* <div className="header-subtitle">
+            <h2>Learn Something new everyday in 3 mins and get closer to becoming a generalist!</h2>
+          </div> */}
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+      <div className="container">
+        
+        <div className="topic-title">
+            <div>Today's topic is</div>
+            <div className='topic'> CRISPR.</div>
+        </div>
+        
+        { !apiOutput ? <div className="prompt-container">
+        <div className='question-row-1'>
+          <p className='question-1 yes-no'>Have you heard this term before?</p>
+          <button className='yes-btn' onClick={onYesBtnClick} style={{background:yesClicked?"green":"grey"}}>
+            Yes
+          </button>
+          <button className='no-btn' onClick={onNoBtnClick} style={{background:noClicked?"red":"grey"}}>
+            No
+          </button>
+        </div>
+          <div className='question-row-1 question-age'>
+            <p>Teach me this as I am _ _ years old</p>
+            {/* <textarea
+            placeholder="Eg: 12"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          /> */}
+        
+          <input 
+          type='number' 
+          min='1' 
+          max='99' 
+          className="prompt-box" 
+          onChange={onUserChangedText} 
+          value={userInput} 
+          placeholder=" Eg: 12">
+          </input>
           </div>
-        </a>
+          <div className='question-row-1'>
+          <p className='question-1'>What's your most knowledgeable field?</p>
+          <input
+            placeholder=" Eg: Football"
+            className="prompt-box prompt-field"
+            value={userField}
+            onChange={onUserChangedField}
+          />
+        </div>
+           
+          <div className="prompt-buttons">
+            <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+              <div className="generate">
+                {isGenerating ? <span class="loader"></span> : <p>Teach Me 🙏</p>}
+              </div>
+            </a>
+          </div> 
+        </div> :
+        
+          <div className="output">
+            {/* <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div> */}
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+            <div className ='output-row'>
+            <div className='countdown-text'>
+              Next WhatNot in 
+            </div>
+            <Countdown 
+              date='2022-12-22T00:00:00'
+              renderer={renderer}
+              zeroPadTime= '2'
+             />
+            <div className='share-btn'>
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" target="_blank" rel="noopener noreferrer" data-text="I just learn about CRISPR on WhatNot!" data-hashtags="whatnot " data-lang="en" data-show-count="false">Tweet</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+            </div>
+          </div>}
+        
+      </div>
+      <div className='footer'>
+        Built by <a href='https://twitter.com/SuveenE'>Suveen</a>
       </div>
     </div>
   );
